b6928e5efdf5b6e67e251fb4570b06678232cc3f
setarehkarimi <45576570+setarehkarimi@users.noreply.github.com>
2018-12-06 12:30:16 -0800

    Add files via upload
    Hi there!
    This files are totally new .So you can delete previous ones and replace these.
    Also I made changes to menu item to List item as Mehrnaz said.

--- navbar-script.js
+++ navbar-script.js
@@ -1,16 +1,28 @@
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "topnav";
+    }
+
+
 }
 
+function showList(obj)
+{
+    if (parseInt(window.innerWidth) >=600){
+    document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+}
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+    if (!e.target.matches('.dropbtn')) {
+      var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+          myDropdown.classList.remove('show');
+        }
+    }
   }
-}
